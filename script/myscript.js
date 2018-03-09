df7ee108671fd7d6f44be3feca67cacb1e0c927b
WasMachenSachen <loges.info@googlemail.com>
2018-03-09 20:18:06 +0100

    aufgeräumt

--- script/myscript.js
+++ script/myscript.js
@@ -1,23 +1,62 @@
+var aktuellesSpiel = 0;
+var data;
+document.addEventListener("DOMContentLoaded", function(event) {
+  var url = window.location.search;
+  url = url.replace("?spiel=", ''); // remove the ?spiel=
+  if(url == ''){aktuellesSpiel = 0;}
+  else{aktuellesSpiel = url;}
+
+var request = new XMLHttpRequest();
+request.open('GET', 'https://gist.githubusercontent.com/WasMachenSachen/2f0e8c10085e90a86487f4025e78d3f5/raw/b390e698d38643b08d1e851591dd7c08cc995c4c/spiele.json', true);
+
+  request.onload = function() {
+    if (request.status >= 200 && request.status < 400) {
+      // Success!
+      data = JSON.parse(request.responseText);
+      array = data;
+      console.log(data.alle_spiele);
+
+      for (var i = 0; i < data.alle_spiele.length; i++) {
+          var zwischen = '</tr><tr class="spielliste_content"><td>'+
+                          data.alle_spiele[i].Name_der_Entwickler+
+                          '</td><td>'+
+                          data.alle_spiele[i].Spiel_title+
+                          '</td><td><a href="'+
+                          data.alle_spiele[i].scratch_link+
+                          '">Jetzt spielen</a></td><td><a href="'+
+                          data.alle_spiele[i].download_link+
+                          '">Download</a></td></tr>';
+        document.querySelector('#spielliste_table').innerHTML += zwischen;
+      }
+
+    } else {
+      // We reached our target server, but it returned an error
+    }
+
+  spielErsteller();
+  };
+
+  request.onerror = function() {
+    // There was a connection error of some sort
+  };
+  request.send();
+
+});
+
 function spielAnzeigen(){
   document.querySelector('#spielliste').classList.toggle("hidden");
 }
+function nächstesSpiel(){
+  if(aktuellesSpiel < data.alle_spiele.length-1){aktuellesSpiel ++;}
+  document.getElementById('nächstesSpiel').href = 'woche_eins.html?spiel='+aktuellesSpiel;
+}
+function letztesSpiel(){
+  if(aktuellesSpiel > 0){aktuellesSpiel --;}
+  document.getElementById('letztesSpiel').href = 'woche_eins.html?spiel='+aktuellesSpiel;
+}
+function spielErsteller() {
+  document.getElementById('spiel_ersteller').innerHTML = data.alle_spiele[aktuellesSpiel].Name_der_Entwickler;
+  document.getElementById('btn_scratch-link').href = data.alle_spiele[aktuellesSpiel].scratch_link;
+  document.getElementById('btn_download-link').href = data.alle_spiele[aktuellesSpiel].download_link;
 
-
-var request = new XMLHttpRequest();
-request.open('GET', 'https://gist.githubusercontent.com/WasMachenSachen/2f0e8c10085e90a86487f4025e78d3f5/raw/b84a25d6ac8ec1394c7eedb35d93f0d8330392ce/spiele.json', true);
-
-request.onload = function() {
-  if (request.status >= 200 && request.status < 400) {
-    // Success!
-    var resp = request.responseText;
-  } else {
-    // We reached our target server, but it returned an error
-
-  }
-};
-
-request.onerror = function() {
-  // There was a connection error of some sort
-};
-
-request.send();
+}
